9ecfc5688d4485decbe1d5b44790b441adc516ac
srabahia <sabri.rabahia@sts-sio-caen.info>
2025-12-17 14:24:30 +0100

    Page d'accueil

---
+++ front/src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card">
+        <h1>{{ title }}</h1>
+        <h2>{{ description }}</h2>
+    </div>
+</template>
+
+
+<script setup >
+    import { defineProps } from 'vue'
+    defineProps({
+    title: String,
+    description: String
+    })
+</script>
+
+<style>
+    .card {
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        padding: 16px;
+        max-width: 300px;
+        box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.1);
+        margin: 10px;
+    }
+    h1 {
+        font-size: 1.5em;
+        margin-bottom: 8px;
+    }
+    h2 {
+        font-size: 1em;
+        color: #666;
+    }
+
+</style>
